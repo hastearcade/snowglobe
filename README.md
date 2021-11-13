8617d56b0ebc5c62640e86951fb8499a733b0779
Keith LaForce <klaforce@gmail.com>
2021-11-13 06:54:03 -0500

    working on documentation

--- README.md
+++ README.md
@@ -12,7 +12,21 @@ Install snowglobe using NPM:
 npm i snowglobe
 ```
 
-## Usage
+## About
+
+Snowglobe is an orchestrator designed to solve state syncronization between a game server and a set of game clients. In a perfect network, maintaining the state of your game world between a set of parties would simply be a matter of applying updates to the game world to all parties simultaneously, but we do not live in a perfect world.
+
+The networking and physics libraries required to build a game or simulation are outside the scope of Snowglobe. Snowglobe will orchestrate the game sync algorithm by utilizing networking libraries like [Geckos.io](https://github.com/geckosio/geckos.io) and physics libraries like [Rapier](https://rapier.rs/).
+
+The problem becomes non-trivial when layering in latency, bad actors, and disconnects. Glenn Fiedler describes the problems and solutions [here](https://gafferongames.com/post/introduction_to_networked_physics/).
+
+Snowglobe assists in solving the difficulty of networking physics for Typescript based games or simulations by implementing Client-side prediction, Server reconciliation, and Display State interpolation. The Ernest Wong and the Crystal Orb team did an excellent job describe the solution as:
+
+- Client-side prediction. Clients immediately apply their local input to their simulation before waiting for the server, so that the player's inputs feel responsive.
+- Server reconciliation. Server runs a delayed, authoritative version of the simulation, and periodically sends authoritative snapshots to each client. Since the server's snapshots represent an earlier simulation frame, each client fast-forwards the snapshot they receive until it matches the same timestamp as what's being shown on screen. Once the timestamps match, clients smoothly blend their states to the snapshot states.
+- Display state interpolation. The simulation can run at a different time-step from the render framerate, and the client will automatically interpolate between the two simulation frames to get the render display state.
+
+## Quick Start
 
 Below is a (non-comprehensive) sample of the ported CrystalOrb API. This example is missing critical features like a game loop, networking, serialization, etc.
 
@@ -57,4 +71,10 @@ const server = new Server(makeWorld(), config, 0)
 
 ## Examples
 
-Currently only the standalone example has been ported. You can run it with `npm run example:standalone`.
+Currently only the standalone example has been ported. You can run it with `npm run example:standalone` and you can see the code [here](https://github.com/3mcd/snowglobe/blob/main/examples/standalone.ts)
+
+## Code Architecture
+
+The following diagram shows a visual representation of the code architecture. The diagram is meant to be understood by reading along side with `examples/standalone.ts`.
+
+![Snowglobe Diagram](./docs/architecture.png)
--- examples/standalone.ts
+++ examples/standalone.ts
@@ -8,18 +8,38 @@ import { DisplayState, Snapshot, World } from "../src/world"
 import { Tweened } from "../src/display_state"
 import { makeMockNetwork } from "../test/mock_network"
 
+// A Snowglobe command represents a player or server
+// issuing an instruction to the game world.
+// This could be moving left or spawning new NPCS. The game world
+// will need to have the command processed which will result in
+// a change of world state. This world state change will then
+// need to be syncronized amongst the other clients.
 type MyCommand = Command & { kind: "accelerate" | "decelerate" | "cheat" }
 
+// A snapshot is the minimal data object representing the
+// entire physics simulation. The goal should be to keep
+// the size of your snapshots as small as possible to reduce
+// the load on the network.
 type MySnapshot = Snapshot & {
   position: number
   velocity: number
 }
 
+// Display State is a representation of what the Player
+// will ultimately see. The entire game world may not need
+// to be seen by the player, and keeping a separate abstraction
+// for Display State helps reduce interpolation complexity
+// when the Server and Client are syncing.
 type MyDisplayState = DisplayState & {
   position: number
   velocity: number
 }
 
+// The interplate function is utilized by Snowglobe to smooth
+// the changes in state between client and server. The server acts
+// as an authoritive player and the clients need to adhere to
+// snapshots that are sent by the server. The clients will utilize
+// this function to perform linear interpolation or lerping.
 function interpolate(
   state1: MyDisplayState,
   state2: MyDisplayState,
@@ -34,20 +54,49 @@ function interpolate(
   }
 }
 
+// constants used by the game loop
 const TIMESTEP_SECONDS = 1 / 60
 const TIMESTEP_MS = TIMESTEP_SECONDS * 1000
+
+// MyWorld respresents a Snowglobe world. Every Snowglobe
+// world must have an associated type for Commands and
+// Snapshots. The Snowglobe library will be responsible
+// for syncing commands and snapshots between the client and server.
+// These commands and snapshots assist in the following:
+/*
+- Client-side prediction. Clients immediately apply their local 
+input to their simulation before waiting for the server, 
+so that the player's inputs feel responsive.
+- Server reconciliation. Server runs a delayed, authoritative version 
+of the simulation, and periodically sends authoritative snapshots to 
+each client. Since the server's snapshots represent an earlier simulation 
+frame, each client fast-forwards the snapshot they receive until it matches 
+the same timestamp as what's being shown on screen. Once the timestamps 
+match, clients smoothly blend their states to the snapshot states.
+*/
 
 class MyWorld implements World<MyCommand, MySnapshot> {
   private position = 0
   private velocity = 0
   private cachedMomentum: number | undefined
 
+  // Step is run on every tick of the game or simuation
+  // This code should perform any integration with
+  // your physics library of choice to update the state
+  // of the game world. For our example, we are simply
+  // updating the position of our single game object.
   step() {
     const MASS = 2
     this.position += this.velocity * TIMESTEP_SECONDS
     this.cachedMomentum = this.velocity * MASS
   }
 
+  // All world commands run on both the Client and the Server
+  // commandIsValid will allow the authoritative server to
+  // only execute commands defined by the developer and even
+  // gives the ability to have special commands for the server only.
+  // In this example, only the server is allowed to send the 'cheat'
+  // command to the simulation for processing.
   commandIsValid(command: MyCommand, clientId: number) {
     if (command.kind === "cheat") {
       return clientId === 42
@@ -55,6 +104,14 @@ class MyWorld implements World<MyCommand, MySnapshot> {
     return true
   }
 
+  // The apply command function will interprete the desired
+  // action and update the simulation/world state accordingly
+  // generally speaking, the developer should not update the
+  // state of the world directly for things like position, velocity,
+  // etc. The developer should maintain a separate model for the world
+  // and update variables in that state. Then the step function should
+  // call the physics engine 'tick' and have the engine update the true
+  // state of the world.
   applyCommand(command: MyCommand) {
     switch (command.kind) {
       case "accelerate":
@@ -69,12 +126,22 @@ class MyWorld implements World<MyCommand, MySnapshot> {
     }
   }
 
+  // Each client will be reconciled with snapshots provided by
+  // the authoritative server. The interpolate function defined
+  // above is used by Snowglobe to tween between two DisplayStates
+  // Those results are then sent as Snapshots to the client and
+  // are applied here. The position and velocity here are
+  // not the direct values provided by the server because that would
+  // cause 'flickering' or the world objects might appear to jerk around.
+  // Thus, the values provided to this function are interpolated.
   applySnapshot(snapshot: MySnapshot) {
     this.position = snapshot.position
     this.velocity = snapshot.velocity
     this.cachedMomentum = undefined
   }
 
+  // return a minimal representation of your physical world
+  // The smaller, the better to prevent network congestion.
   snapshot() {
     return {
       position: this.position,
@@ -85,6 +152,9 @@ class MyWorld implements World<MyCommand, MySnapshot> {
     }
   }
 
+  // return a minimal representation of what will be displayed to
+  // the player. If a world object is not visible, it should not
+  // be returned from this function.
   displayState() {
     return {
       position: this.position,
